8d8c043c4431a538d0692470ded23fa207121a63
christian cedillo <christian.cedillo@octopy.com>
2022-09-05 18:58:29 -0500

    fix: instructions were added to execute the code

--- HolaEquipo.c
+++ HolaEquipo.c
@@ -1,3 +1,9 @@
+//Para ejecutar este codigo, abrir en el navegador 
+//y poner la siguiente url: https://www.onlinegdb.com/online_c_compiler
+//copiar este codigo y pegarlo en el editor de textos online
+//en la parte superior habra un boton de play y asi podras ver
+//nuestros nombres :D
+
 
 #include <stdio.h>
 
